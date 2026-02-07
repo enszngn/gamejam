418ae2daaf217b1d4a964890c4d1d73434761bed
Enes Zengin <eneszengin2006@outlook.com>
2026-02-07 17:03:50 +0300

    duzeltme

---
+++ game_manager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+# Oyuncunun yeni sahnede hangi ID'ye sahip kapıda doğacağını tutar
+var hedef_kapi_id: String = ""
---
+++ scripts/room1.gd
@@ -0,0 +1,16 @@
+extends LevelManager 
+# Gördüğünüz gibi Node2D yerine LevelManager yazdık.
+# Artık Room1, LevelManager'ın tüm özelliklerine (ışınlanma koduna) sahip!
+
+func _ready():
+	# BURASI ÇOK KRİTİK!
+	# LevelManager'daki _ready'nin çalışması için 'super()' çağırmalıyız.
+	super._ready() 
+	
+	# Buradan aşağısı sadece Room 1'e özel kodlar
+	print("Oda 1 Yüklendi! Buraya özel düşmanlar doğabilir.")
+	music_cal_oda1()
+
+func music_cal_oda1():
+	# Oda 1'e özel müzik kodu vs.
+	pass
