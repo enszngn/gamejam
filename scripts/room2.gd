4bd84f089ad86ca109d3caf174d271cdd5136fee
CagriYucel <alpcagriyucel@gmail.com>
2026-02-07 16:06:52 +0300

    Update room2.gd

--- scripts/room2.gd
+++ scripts/room2.gd
@@ -1,11 +1,16 @@
-extends Node2D
+extends LevelManager 
+# Gördüğünüz gibi Node2D yerine LevelManager yazdık.
+# Artık Room1, LevelManager'ın tüm özelliklerine (ışınlanma koduna) sahip!
 
+func _ready():
+	# BURASI ÇOK KRİTİK!
+	# LevelManager'daki _ready'nin çalışması için 'super()' çağırmalıyız.
+	super._ready() 
+	
+	# Buradan aşağısı sadece Room 1'e özel kodlar
+	print("Oda 2 Yüklendi! Buraya özel düşmanlar doğabilir.")
+	music_cal_oda1()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func music_cal_oda1():
+	# Oda 1'e özel müzik kodu vs.
 	pass
