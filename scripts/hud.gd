27131693111afb1f8bf2ef721149ecdace1e18fc
Enes Zengin <eneszengin2006@outlook.com>
2026-02-07 19:00:57 +0300

    hud

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,11 +1,20 @@
-extends CanvasGroup
+extends CanvasLayer
 
-#gecici olarak money ve oil miktari 0
-@export var oil_label: Label
-@export var money_label: Label
+@onready var oil_label = $Control/HBoxContainer/oilLabel
+@onready var money_label = $Control/HBoxContainer/moneyLabel
 
-func update_oil(amount):
-	oil_label.text = "Oil: " + str(amount)
+func _ready():
+	# A. Set the text immediately when the game starts (so it doesn't say "Label")
+	update_oil_text(GameManager.oil)
+	update_money_text(GameManager.money)
+	
+	# B. CONNECT TO SIGNALS (The most important part)
+	# Syntax: Who.SignalName.connect(FunctionToRun)
+	GameManager.oil_changed.connect(update_oil_text)
+	GameManager.money_changed.connect(update_money_text)
 
-func update_money(amount):
-	oil_label.text = "Money: " + str(amount)
+func update_oil_text(new_amount):
+	oil_label.text = "Oil: " + str(new_amount)
+
+func update_money_text(new_amount):
+	money_label.text = "Money: " + str(new_amount)
