9fa87474ca4f1e7eb0272172bcc82246a83526e6
Enes Zengin <eneszengin2006@outlook.com>
2026-02-08 00:15:09 +0300

    son commit

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -3,7 +3,7 @@ extends Node
 #as a good practice, variables about money and oil should be here, disconnected from everything else
 var oil: int = 0
 var money: int = 0
-var oil_storage: int = 100
+var oil_storage: int = 1000
 
 #i've also learned that rather than updating the amount of oil and money every frame,
 #making our other scripts scream OIL_CHANGED everytime oil changes is a better way to do it.
---
+++ scripts/full_tank_alert.gd
@@ -0,0 +1,6 @@
+extends Sprite2D
+
+@export var percentage_trigger: float = 0.95
+
+func _ready():
+	visible = false
